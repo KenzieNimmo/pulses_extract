51536b26111d5f9d900e254906f4d953294fe0ec
Daniele Michilli <danielemichilli@gmail.com>
2016-09-21 15:31:08 +0200

    Bugs solved

--- auto_waterfaller.py
+++ auto_waterfaller.py
@@ -10,16 +10,25 @@ import rfifind
 import psrfits
 import spectra
 
-def main(fits, time, DM, directory=''):# (fits, time, DM, time_resolution=81.9e-6, )
+def main(fits, time, DM, directory='', FRB_name='FRB121102'):
     if directory != '':
     	directory += '/'
     else:
     	pass
 
 	rawdata = psrfits.PsrfitsFile(fits)
-	#time_resolution = 81.9e-6 #only need this if need downsamp
-	#Downsamp = time[0]/sample[0]/time_resolution
 	count = 0
+	observation = str(fits)[:-5]
+
+	#Open header of the fits file
+	with psrfits.pyfits.open(fits, memmap=True) as fn:
+  		header = fn['SUBINT'].header + fn['PRIMARY'].header
+
+	#MJD of the beginning of the observation
+	start_MJD = header['STT_IMJD'] + header['STT_SMJD'] / 86400. 
+
+	#MJD of the pulses
+	pulse_MJD = start_MJD + time / 86400.
 
 	for i in time: 
 		start_time = i - 0.01
@@ -28,14 +37,14 @@ def main(fits, time, DM, directory=''):# (fits, time, DM, time_resolution=81.9e-
 				scaleindep=False, width_bins=1, mask=False, maskfn=None,\
 				bandpass_corr=False, ref_freq=None)
 
+		plt.figure()
 		plot_waterfall(data, start, 0.03, 
 	                   integrate_ts=True, integrate_spec=False, show_cb=False, 
 	                   cmap_str="gist_yarg", sweep_dms=[], sweep_posns=[],
 	                   ax_im=None, ax_ts=None, ax_spec=None, interactive=False)
 
-		count += 1
-		#plt.title()
-		plt.savefig('%stest_burst_%d_zoomed.png'%(directory, count))
+		plt.suptitle('%s %0.8f [close up]\n %s'%(FRB_name, pulse_MJD[count], observation), y=1.05)
+		plt.savefig('%s%s_%0.8f_zoomed.png'%(directory, FRB_name, pulse_MJD[count]), bbox_inches='tight', pad_inches=0.1)
 
 		#Zoomed out version
 		start_time = i - 0.05
@@ -44,21 +53,22 @@ def main(fits, time, DM, directory=''):# (fits, time, DM, time_resolution=81.9e-
 				nbins=None, nsub=None, subdm = DM, zerodm=False, downsamp=1,\
 				scaleindep=False, width_bins=1, mask=False, maskfn=None,\
 				bandpass_corr=False, ref_freq=None)
-
+		plt.figure()
 		plot_waterfall(data, start, 0.1, 
 	                   integrate_ts=True, integrate_spec=False, show_cb=False, 
 	                   cmap_str="gist_yarg", sweep_dms=[], sweep_posns=[],
 	                   ax_im=None, ax_ts=None, ax_spec=None, interactive=False)
-
-		plt.savefig('%stest_burst_%d.png'%(directory, count))
-		#plt.savefig('test_burst_%d.png'%count)
-
+		
+		plt.suptitle('FRB121102 %0.8f \n %s'%(pulse_MJD[count],observation), y=1.05)
+		plt.savefig('%sFRB121102_%0.8f.png'%(directory, pulse_MJD[count]), bbox_inches='tight', pad_inches=0.1)
+		plt.close('all')
+		count += 1
 
 
 
 
 
 if __name__ == '__main__':
-	DM, time, sample = np.loadtxt(sys.argv[2], usecols=(0,2,3), unpack=True) #can simplify if don't need downsamp info
+	DM, time, sample = np.loadtxt(sys.argv[2], usecols=(0,2,3), unpack=True)
 
-	main(sys.argv[1],time,DM) #
+	main(sys.argv[1],time,DM)
