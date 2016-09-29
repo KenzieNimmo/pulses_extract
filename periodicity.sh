502be3a24a88138239f60801cb54942b66dc53bd
Daniele Michilli <danielemichilli@gmail.com>
2016-09-29 16:46:43 +0200

    Bugs solved

--- periodicity.sh
+++ periodicity.sh
@@ -4,8 +4,8 @@ if [ $# -ne 2 ]; then
    echo "Script for periodicity search on Arecibo FRB121102 data."
    echo ""
    echo "Usage: sh periodicity.sh output_folder fits_filename"
-   echo "fits_filename contains the full path of the file."
    echo "output_folder is the directory to store the plots."
+   echo "fits_filename contains the full path of the file."
    echo "The script assumes that meta files are in a TEMP subdirectory."
    exit
 fi
