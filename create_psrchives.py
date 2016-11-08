e8e82a745b9f3be63f4296cdb48df351a0e599cf
Daniele Michilli <danielemichilli@gmail.com>
2016-11-08 12:58:22 +0100

    Bugs solved

--- create_psrchives.py
+++ create_psrchives.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
   fits_path = os.path.join(args.obsPATH, '{}', args.fits_file)
     
   for idx_p, puls in pulses.iterrows():
-    fits_file = fits_path.format(idx_p)
+    fits_file = fits_path.format(idx_p) 
     if '*' in fits_file: fits_file = glob(fits_file)[0]
     dspsr(puls, par_file, fits_file, profile_bins=args.profile_bins)
 
