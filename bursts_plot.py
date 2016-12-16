8a27a231eb6a3a1516ff971b846b1738b621f02f
Daniele Michilli <danielemichilli@gmail.com>
2016-12-16 15:50:34 +0100

    Bugs solved

--- bursts_plot.py
+++ bursts_plot.py
@@ -202,7 +202,7 @@ def plot(DS, subplot_spec, fig, extent=None, ncols=1, nrows=1, t_scrunch=1., f_s
   #Plot components
   colors = ['dodgerblue', 'mistyrose', 'sage', 'lavender', 'y', 'lightsalmon', 'silver']
   for i in range(len(components)-1):
-    ax2.axvspan(components[i], components[i+1], c=colors[i], ls='-', linewidth=.2, alpha=.5)
+    ax2.axvspan(components[i], components[i+1], color=colors[i], ls='-', linewidth=.2, alpha=.5)
     bl_c = np.mean(smooth_DS[:, components[i] : components[i+1]], axis=1)
     ax3.plot(bl_c, y, ls='--', c=colors[i])
     
