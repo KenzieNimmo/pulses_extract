fc953e49365da301b114cfd9092427266e2ef96e
Daniele Michilli <danielemichilli@gmail.com>
2016-12-17 19:12:51 +0100

    Extract archives at given times

--- create_psrchives.py
+++ create_psrchives.py
@@ -19,7 +19,7 @@ ephemeris = '''PSRJ J0531+33
 RAJ 05:31:58.561
 DECJ 33:08:52.56
 PEPOCH 57388.0
-P0 0.04194304
+P0 {}
 EPHVER 2
 DM {}'''
 
@@ -48,7 +48,24 @@ def read_fits(fits_file):
   return {'time_resolution': time_resolution, 'freq_c': freq_c, 'bandwidth': bandwidth}
 
   
-def dspsr(puls, par_file, fits_file, profile_bins=4096, parallel=False):
+def dspsr(fits_file, puls=None, par_file=False, profile_bins=4096, parallel=False, DM=560.5, Downfact=False, SMJD=False, width=0.04194304):
+  #Read puls
+  if puls:
+    DM = puls.DM
+    SMJD = puls.SMJD
+    Downfact = puls.Downfact
+  else:
+    if not SMJD:
+      print "Either a valid pulse pandas instance or seconds from beginning of day (SMJD) required. Exiting"
+      return 1
+    
+  #Read par_file
+  if not par_file:
+    obs_id = os.path.splitext(os.path.basename(fits_file))[0]
+    par_file = '/dev/shm/{}_ephemeris'.format(obs_id)
+    with open(par_file, 'w') as f:
+      f.write(ephemeris.format(width, DM))
+    
   archive_name = os.path.splitext(fits_file)[0]
   puls_folder = os.path.split(archive_name)[0]
   if not os.path.isfile(archive_name + '.ar'):
@@ -56,7 +73,7 @@ def dspsr(puls, par_file, fits_file, profile_bins=4096, parallel=False):
     period = readfile['time_resolution'] * profile_bins
 
     #Delay between maximum and central frequencies
-    DM_delay = psr_utils.delay_from_DM(puls.DM, readfile['freq_c']) - psr_utils.delay_from_DM(puls.DM, readfile['freq_c'] + readfile['bandwidth'] / 2. )
+    DM_delay = psr_utils.delay_from_DM(DM, readfile['freq_c']) - psr_utils.delay_from_DM(DM, readfile['freq_c'] + readfile['bandwidth'] / 2. )
     n_puls = int(DM_delay / period)
 
     temp_folder = os.path.join('/dev/shm', os.path.basename(archive_name))
@@ -80,7 +97,7 @@ def dspsr(puls, par_file, fits_file, profile_bins=4096, parallel=False):
       archive_time_list = archive_time_list[idx_sorted]
     
       #Find archive where dispersed pulse would start
-      start_dispersed_puls = puls.SMJD - archive_time_list
+      start_dispersed_puls = SMJD - archive_time_list
       idx_puls = np.where( (start_dispersed_puls > 0) & (start_dispersed_puls < period))[0][0]
     
       #Check that puls is centered
@@ -103,18 +120,19 @@ def dspsr(puls, par_file, fits_file, profile_bins=4096, parallel=False):
     subprocess.call(['paz', '-e', 'ar.paz', '-r', archive_name + '.ar'], cwd=puls_folder)
   
   #Create downsampled archive at the closest factor scrunched in polarisation
-  downfact = int(puls.Downfact)
-  while profile_bins % downfact != 0:
-    downfact -= 1
-  
-  if not os.path.isfile(archive_name + '.ar.paz.pb'+str(downfact)):  
-    subprocess.call(['pam', '-e', 'paz.pb'+str(downfact), '-p', '-b', str(downfact), archive_name + '.ar.paz'], cwd=puls_folder)  
-    #Plot the archive
-    psrchive_plots(os.path.join(puls_folder, archive_name + '.ar.paz.pb'+str(downfact)))
-  
-  #Create compressed archive
-  if not os.path.isfile(archive_name + '.ar.paz.Fpb'+str(downfact)):
-    subprocess.call(['pam', '-e', 'Fpb'+str(downfact), '-F',  archive_name + '.ar.paz.pb'+str(downfact)], cwd=puls_folder)  
+  if Downfact:
+    downfact = int(Downfact)
+    while profile_bins % downfact != 0:
+      downfact -= 1
+    
+    if not os.path.isfile(archive_name + '.ar.paz.pb'+str(downfact)):  
+      subprocess.call(['pam', '-e', 'paz.pb'+str(downfact), '-p', '-b', str(downfact), archive_name + '.ar.paz'], cwd=puls_folder)  
+      #Plot the archive
+      psrchive_plots(os.path.join(puls_folder, archive_name + '.ar.paz.pb'+str(downfact)))
+    
+    #Create compressed archive
+    if not os.path.isfile(archive_name + '.ar.paz.Fpb'+str(downfact)):
+      subprocess.call(['pam', '-e', 'Fpb'+str(downfact), '-F',  archive_name + '.ar.paz.pb'+str(downfact)], cwd=puls_folder)  
 
   return
 
@@ -133,13 +151,8 @@ if __name__ == '__main__':
   for idx_p, puls in pulses.iterrows():
     fits_file = fits_path.format(idx_p) 
     if '*' in fits_file: fits_file = glob(fits_file)[0]
-    
-    obs_id = os.path.splitext(os.path.basename(args.db_name))[0]
-    par_file = '/dev/shm/{}_ephemeris'.format(obs_id)
-    with open(par_file, 'w') as f:
-      f.write(ephemeris.format(puls.DM))
-    
-    dspsr(puls, par_file, fits_file, profile_bins=args.profile_bins)
+
+    dspsr(fits_file, puls=puls, profile_bins=args.profile_bins)
     
     os.remove(par_file)
 
