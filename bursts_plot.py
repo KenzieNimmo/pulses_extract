98219462177c71a26ee25145ed9afe64aa49fa9e
Daniele Michilli <danielemichilli@gmail.com>
2017-01-09 19:35:34 +0100

    Polarisation plot

--- bursts_plot.py
+++ bursts_plot.py
@@ -16,20 +16,21 @@ def parser():
     # Command-line options
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                     description="Plot dynamic spectrum from multiple archives with 1 polarisation and 1 subintegration.")
-    parser.add_argument('archives_list', help="Name of the psrchive files to plot.", nargs='+')
+    parser.add_argument('archives_list', help="Name of the psrchive files to plot, scrunched in time and polarisation.", nargs='+')
     parser.add_argument('-o', help="Plot name.", default='bursts.png')
     parser.add_argument('-show', help="Show the plot.", action='store_false')
     parser.add_argument('-save_fig', help="Save the plot.", action='store_true')
     parser.add_argument('-zap', help="Plot to manually zap bins out.", action='store_true')
     parser.add_argument('-ncols', help="Number of columns in the general plot.", default=1, type=int)
     parser.add_argument('-nrows', help="Number of rows in the general plot.", default=1, type=int)
-    parser.add_argument('-t_scrunch', help="Time scrunch archives by this factor.", default=1., type=float)
-    parser.add_argument('-f_scrunch', help="Frequency scrunch archives by this factor.", default=1., type=float)
+    parser.add_argument('-t_scrunch', help="Time scrunch archives by this factor.", default=1., type=int)
+    parser.add_argument('-f_scrunch', help="Frequency scrunch archives by this factor.", default=1., type=int)
     parser.add_argument('-time_window', help="Time window around the burst in ms.", default=False, type=float)
     parser.add_argument('-f_min', help="Minimum frequency to plot in GHz.", default=None, type=float)
     parser.add_argument('-f_max', help="Maximum frequency to plot in GHz.", default=None, type=float)
     parser.add_argument('-cmap', help="Colormap to use in the plots. Other useful: RdGy_r", default='Greys')
     parser.add_argument('-log_scale', help="Logaritmic color scale.", action='store_true')
+    parser.add_argument('-pol', help="Plot polarisation information.", action='store_true')
     return parser.parse_args()
 
 
@@ -39,6 +40,11 @@ def main():
   args = parser()
   plot_grid = gridspec.GridSpec(args.nrows, args.ncols)  #Grid of burst plots
   fig = plt.figure(figsize=(4*8.27, 4*11.69))  #A4
+  
+  #Zapping mode
+  if args.zap:
+    args.t_scrunch = args.f_scrunch = False
+    args.pol = args.time_window = False
   
   #Load archive list
   if len(args.archives_list) == 1: ar_list = glob(args.archives_list[0])
@@ -53,13 +59,9 @@ def main():
       if args.ncols - idx == plots_to_skip: skip += plots_to_skip
       
     #Load archive
-    DS, extent = load_DS(archive_name)
-    
+    DS, spectrum, ts, extent = load_DS(archive_name, args.pol, t_scrunch=args.t_scrunch, f_scrunch=args.f_scrunch)
     components = burst_components(archive_name)
-
-    #Zap the archive
     if args.zap: extent = None
-    zap(archive_name, DS)
     
     #Invert the band if flipped
     if extent and (extent[3] < extent[2]):
@@ -75,9 +77,10 @@ def main():
       plot_DM_curves(extent, plot_grid[idx], fig, fmin=args.f_min, fmax=args.f_max, width=args.time_window)
       skip += 1
       idx += 1
-
-    plot(DS, plot_grid[idx], fig, extent=extent, ncols=args.ncols, nrows=args.nrows, t_scrunch=args.t_scrunch, f_scrunch=args.f_scrunch,\
-         index=idx, width=args.time_window, fmin=args.f_min, fmax=args.f_max, cmap=args.cmap, log_scale=args.log_scale, components=components)
+    
+    plot(DS, spectrum, ts, extent, plot_grid[idx], fig, extent=extent, ncols=args.ncols, nrows=args.nrows,\
+         index=idx, width=args.time_window, fmin=args.f_min, fmax=args.f_max, cmap=args.cmap, log_scale=args.log_scale, components=components,\
+         pol=args.pol, zap=args.zap)
 
   
   #General plot settings
@@ -128,56 +131,54 @@ def plot_DM_curves(extent, subplot_spec, fig, fmin=None, fmax=None, width=False)
   return
   
   
-def plot(DS, subplot_spec, fig, extent=None, ncols=1, nrows=1, t_scrunch=1., f_scrunch=1., index=None, width=False, fmin=None, fmax=None, cmap='Greys', log_scale=False, components=None):
+def plot(DS, spectrum, ts, extent, subplot_spec, fig, extent=None, ncols=1, nrows=1, t_scrunch=1., f_scrunch=1., index=None,\
+    width=False, fmin=None, fmax=None, cmap='Greys', log_scale=False, components=None, zap=False):
   #Define subplots
   plot_grid = gridspec.GridSpecFromSubplotSpec(2, 2, subplot_spec, wspace=0., hspace=0., height_ratios=[1,5], width_ratios=[5,1])
   ax1 = plt.Subplot(fig, plot_grid[2])
   ax2 = plt.Subplot(fig, plot_grid[0], sharex=ax1)
   ax3 = plt.Subplot(fig, plot_grid[3], sharey=ax1)
 
-  #Dynamic spectrum
-  if extent:
-    smooth_DS = scipy.ndimage.zoom(DS, (1./f_scrunch,1./t_scrunch), order=1)
-    smooth_DS -= np.median(smooth_DS)
-    smooth_DS /= smooth_DS.max()
-    units = ("GHz", "ms")
-  else: 
-    width = False
-    fmin = fmax = None
-    smooth_DS = DS
-    units = ("chan", "bin")
-
-  if extent and not fmin: fmin = extent[2]
-  if extent and not fmax: fmax = extent[3]
-    
   #Applies temporal and frequency windows
-  if extent:
-    prof = np.mean(smooth_DS, axis=0)
-    res_t = extent[1] / prof.size 
-    peak_ms = float(prof.argmax()) * res_t
+  if zap:
+    fmin = fmax = None
+    units = ("chan", "bin")
+  else: units = ("GHz", "ms")
+
+  if not zap and not fmin: fmin = extent[2]
+  if not zap and not fmax: fmax = extent[3]
+  
+  if zap: extent = [0, DS.shape[1]-1, 0, DS.shape[0]-1]
+  else:
+    res_t = extent[1] / ts.size 
+    peak_ms = float(ts.argmax()) * res_t
     extent[0] = - width / 2.
     extent[1] = width / 2.
     components_ms = components / t_scrunch * res_t
     components = (components / t_scrunch).astype(int)
-    if width: 
-      smooth_DS = smooth_DS[:, int(prof.argmax() - np.ceil(width / 2. / res_t)) : int(prof.argmax() + np.ceil(width / 2. / res_t))]
+    if width:
+      t0 = int(ts.argmax() - np.ceil(width / 2. / res_t))
+      t1 = int(ts.argmax() + np.ceil(width / 2. / res_t))
       components_ms -= peak_ms
-      components -= int(prof.argmax() - np.ceil(width / 2. / res_t))
+      components -= int(ts.argmax() - np.ceil(width / 2. / res_t))
+    else: t0 = t1 = None
     
     fmin_bin = int(np.floor((fmin - extent[2]) / (extent[3] - extent[2]) * smooth_DS.shape[0]))
     fmax_bin = int(np.ceil((fmax - extent[2]) / (extent[3] - extent[2]) * smooth_DS.shape[0]))
-    smooth_DS = smooth_DS[fmin_bin:fmax_bin]
     extent[2] = fmin
     extent[3] = fmax
-        
-  else: extent = [0, smooth_DS.shape[1]-1, 0, smooth_DS.shape[0]-1]
+  
+    DS = DS[fmin_bin : fmax_bin, t0 : t1]
+    spectrum = spectrum[fmin_bin : fmax_bin]
+    if len(ts.shape) == 1: ts = ts[t0 : t1]
+    else: ts = ts[:, t0 : t1]
   
   if log_scale:
-    smooth_DS -= smooth_DS.min() + 1e-6
-    smooth_DS /= smooth_DS.max()
-    smooth_DS = np.log(smooth_DS)
+    DS -= DS.min() + 1e-6
+    DS /= DS.max()
+    DS = np.log(DS)
     
-  ax1.imshow(smooth_DS, cmap=cmap, origin='lower', aspect='auto', interpolation='nearest', extent=extent)
+  ax1.imshow(DS, cmap=cmap, origin='lower', aspect='auto', interpolation='nearest', extent=extent)
   
   if width: ax1.set_xlim(-width/2., width/2.)
   ax1.set_ylim(fmin, fmax)
@@ -189,18 +190,22 @@ def plot(DS, subplot_spec, fig, extent=None, ncols=1, nrows=1, t_scrunch=1., f_s
   else: ax1.set_xlabel("Time ({})".format(units[1]))
   
   #Pulse profile
-  prof = np.mean(smooth_DS, axis=0)
-  x = np.linspace(extent[0], extent[1], prof.size)
-  ax2.plot(x, prof, 'k-')
+  if len(ts.shape) == 1: 
+    x = np.linspace(extent[0], extent[1], ts.size)
+    ax2.plot(x, ts, 'k-')
+  else:
+    x = np.linspace(extent[0], extent[1], ts.shape[1])
+    ax2.plot(x, ts[0], 'k-')
+    ax2.plot(x, ts[1], 'r-')
+    ax2.plot(x, ts[2], 'b-')
   ax2.tick_params(axis='y', which='both', left='off', right='off', labelleft='off')
   ax2.tick_params(axis='x', labelbottom='off')
   if width: ax2.set_xlim(-width/2., width/2.)
   else: ax2.set_xlim(extent[0:2])
   
-  #Baseline
-  bl = np.sum(smooth_DS, axis=1)
-  y = np.linspace(extent[2], extent[3], bl.size)
-  ax3.plot(bl, y, 'k-')
+  #Spectrum
+  y = np.linspace(extent[2], extent[3], spectrum.size)
+  ax3.plot(spectrum, y, 'k-')
   ax3.tick_params(axis='x', which='both', top='off', bottom='off', labelbottom='off')
   ax3.tick_params(axis='y', labelleft='off')
   ax3.set_ylim(fmin, fmax)
@@ -210,34 +215,66 @@ def plot(DS, subplot_spec, fig, extent=None, ncols=1, nrows=1, t_scrunch=1., f_s
     colors = ['b', 'r', 'g', 'c', 'm', 'y', 'lime', 'darkblue']
     for i in range(len(components)-1):
       ax2.axvspan(components_ms[i], components_ms[i+1], color=colors[i], ls='-', ymax=.1)
-      bl_c = np.sum(smooth_DS[:, components[i] : components[i+1]], axis=1)
+      bl_c = spectrum[:, components[i] : components[i+1]]
       ax3.plot(bl_c, y, c=colors[i], lw=1.)
   except UnboundLocalError: pass
-    
-    
+
   fig.add_subplot(ax1)
   fig.add_subplot(ax2)
   fig.add_subplot(ax3)
   return 
 
 
-
-def load_DS(archive_name):
+def load_DS(archive_name, pol=False, zap=False, t_scrunch=False, f_scrunch=False):
+  #Load archive
   load_archive = psrchive.Archive_load(archive_name)
   load_archive.remove_baseline()
-  DS = load_archive.get_data().squeeze()
+  load_archive.convert_state('Stokes')
+  if not pol: load_archive.pscrunch()
+  archive = load_archive.get_data().squeeze()
   
-  freq = load_archive.get_centre_frequency()
-  bw = load_archive.get_bandwidth()
-  f0 = (freq - bw / 2) / 1000
-  f1 = (freq + bw / 2) / 1000
-  duration = load_archive.integration_length() * 1000
+  #Zap archive
+  if pol: for i in range(4): zap_ar(archive_name, archive[i])
+  else: zap_ar(archive_name, archive)
   
-  return DS, [0., duration, f0, f1]
+  #Load dynamic spectrum
+  if pol: 
+    DS = archive[0]
+    if t_scrunch: DS = np.sum(np.reshape(DS, (DS.shape[0], DS.shape[1], DS.shape[2] / t_scrunch, t_scrunch)), axis=-1)
+    if f_scrunch: DS = np.sum(np.reshape(DS, (DS.shape[0], DS.shape[1]  / t_scrunch, t_scrunch, DS.shape[2])), axis=2)
+  else: 
+    DS = archive
+    if t_scrunch: DS = np.sum(np.reshape(DS, (DS.shape[0], DS.shape[1] / t_scrunch, t_scrunch)), axis=-1)
+    if f_scrunch: DS = np.sum(np.reshape(DS, (DS.shape[0]  / t_scrunch, t_scrunch, DS.shape[1])), axis=1)
+  
+  #Load frequency spectrum
+  spectrum = np.sum(DS, axis=1)
+  
+  #Load timeseries
+  if pol:   #ORDINE DELLE OPERAZIONI CORRETTO??
+    I = np.sum(DS[0], axis=0)
+    L = np.sum(np.sqrt(DS[1]**2 + DS[2]**2), axis=0)
+    V = np.sum(DS[3], axis=0)
+    PA = np.sum(np.rad2deg(np.arctan2(DS[2] , DS[1])) / 2., axis=0)
+    ts = np.vstack((I, L, V, PA))
+  else:
+    ts = np.sum(DS, axis=0)
+    
+  #Load extensions
+  if zap: extent = None
+  else:
+    freq = load_archive.get_centre_frequency()
+    bw = load_archive.get_bandwidth()
+    f0 = (freq - bw / 2) / 1000
+    f1 = (freq + bw / 2) / 1000
+    duration = load_archive.integration_length() * 1000
+    extent = [0., duration, f0, f1]
+  
+  return DS, spectrum, ts, extent
    
    
    
-def zap(archive_name, DS):
+def zap_ar(archive_name, DS):
   zap_list = load_zap_list(archive_name)
   
   med = np.median(DS)
@@ -250,43 +287,43 @@ def zap(archive_name, DS):
   
 
 def burst_components(archive_name):
-  if os.path.basename(archive_name) == 'puppi_57649_C0531+33_0106_413.Tp':
+  if os.path.basename(archive_name).startswith('puppi_57649_C0531+33_0106_413'):
     components = [1910, 2012, 2120, 2307]
   
-  elif os.path.basename(archive_name) == 'puppi_57644_C0531+33_0021_2461.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57644_C0531+33_0021_2461'):
     components = [2100, 2700]
   
-  elif os.path.basename(archive_name) == 'puppi_57638_C0531+33_1218_280.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57638_C0531+33_1218_280'):
     components = [1100, 1600]
   
-  elif os.path.basename(archive_name) == 'puppi_57648_C0531+33_0048_821.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57648_C0531+33_0048_821'):
     components = [2295, 2434, 2522, 2800]
   
-  elif os.path.basename(archive_name) == 'puppi_57640_C0531+33_1274_1421.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57640_C0531+33_1274_1421'):
     components = [2100, 2600]
   
-  elif os.path.basename(archive_name) == 'puppi_57641_C0531+33_1312_185.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57641_C0531+33_1312_185'):
     components = [1570, 1834, 1969, 2211, 2264, 2361, 2510, 2685]
   
-  elif os.path.basename(archive_name) == 'puppi_57641_C0531+33_1312_521.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57641_C0531+33_1312_521'):
     components = [2368, 2471, 2530, 2750]
   
-  elif os.path.basename(archive_name) == 'puppi_57642_C0531+33_1322_1965.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57642_C0531+33_1322_1965'):
     components = [1847, 1944, 2032, 2124, 2240, 2360]
   
-  elif os.path.basename(archive_name) == 'puppi_57648_C0531+33_0048_378.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57648_C0531+33_0048_378'):
     components = [1187, 1347, 1482]
   
-  elif os.path.basename(archive_name) == 'puppi_57642_C0531+33_1322_7699.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57642_C0531+33_1322_7699'):
     components = [2300, 2385, 2474, 2560, 2825]
   
-  elif os.path.basename(archive_name) == 'puppi_57646_C0531+33_0085_2476.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57646_C0531+33_0085_2476'):
     components = [1714, 1797, 1910, 2070, 2222]
   
-  elif os.path.basename(archive_name) == 'puppi_57646_C0531+33_0085_4275.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57646_C0531+33_0085_4275'):
     components = [626, 753, 890, 1194]
   
-  elif os.path.basename(archive_name) == 'puppi_57638_C0531+33_1218_2797.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57638_C0531+33_1218_2797'):
     components = [1601, 1659, 1735, 1860, 2075]
   
   else:
@@ -304,7 +341,7 @@ def load_zap_list(archive_name):
   None value in time mean all values.
   '''
   
-  if os.path.basename(archive_name) == 'puppi_57649_C0531+33_0106_413.Tp':
+  if os.path.basename(archive_name).startswith('puppi_57649_C0531+33_0106_413'):
     zap_list = [\
 [99,  2700, 3700],
 [103, 1700, 2700],
@@ -316,7 +353,7 @@ def load_zap_list(archive_name):
 [0, None, None]
 ]
     
-  elif os.path.basename(archive_name) == 'puppi_57644_C0531+33_0021_2461.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57644_C0531+33_0021_2461'):
     zap_list = [\
 [0, None, None],
 [286, None, None],
@@ -336,7 +373,7 @@ def load_zap_list(archive_name):
 [510, None, None]
 ]
 
-  elif os.path.basename(archive_name) == 'puppi_57638_C0531+33_1218_280.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57638_C0531+33_1218_280'):
     zap_list = [\
 [102, 3750, None],
 [170, None, None],
@@ -358,7 +395,7 @@ def load_zap_list(archive_name):
 [0, None, None]
 ]
 
-  elif os.path.basename(archive_name) == 'puppi_57648_C0531+33_0048_821.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57648_C0531+33_0048_821'):
     zap_list = [\
 [0  , None, None],
 [1  , None, None],
@@ -407,7 +444,7 @@ def load_zap_list(archive_name):
 [410, None, None]
 ]
   
-  elif os.path.basename(archive_name) == 'puppi_57640_C0531+33_1274_1421.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57640_C0531+33_1274_1421'):
     zap_list = [\
 [100, None, 1000],
 [275, None, None],
@@ -424,7 +461,7 @@ def load_zap_list(archive_name):
 [0, None, None]
 ]
   
-  elif os.path.basename(archive_name) == 'puppi_57641_C0531+33_1312_185.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57641_C0531+33_1312_185'):
     zap_list = [\
 [178, None, None],
 [275, None, None],
@@ -441,7 +478,7 @@ def load_zap_list(archive_name):
 [0, None, None]
 ]
 
-  elif os.path.basename(archive_name) == 'puppi_57641_C0531+33_1312_521.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57641_C0531+33_1312_521'):
     zap_list = [\
 [102, None, 1500],
 [103, None, None],
@@ -459,7 +496,7 @@ def load_zap_list(archive_name):
 [0, None, None]
 ]
 
-  elif os.path.basename(archive_name) == 'puppi_57642_C0531+33_1322_1965.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57642_C0531+33_1322_1965'):
     zap_list = [\
 [0, None, None],
 [99, 3700, None],
@@ -488,7 +525,7 @@ def load_zap_list(archive_name):
 [353, None, None]
 ]
 
-  elif os.path.basename(archive_name) == 'puppi_57648_C0531+33_0048_378.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57648_C0531+33_0048_378'):
     zap_list = [\
 [0, None, None],
 [28 , None, None],
@@ -521,7 +558,7 @@ def load_zap_list(archive_name):
 [324, None, None]
 ]
 
-  elif os.path.basename(archive_name) == 'puppi_57642_C0531+33_1322_7699.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57642_C0531+33_1322_7699'):
     zap_list = [\
 [0, None, None],
 [170, None, None],
@@ -561,7 +598,7 @@ def load_zap_list(archive_name):
 [422, 800, None]
 ]
 
-  elif os.path.basename(archive_name) == 'puppi_57646_C0531+33_0085_2476.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57646_C0531+33_0085_2476'):
     zap_list = [\
 [0, None, None],
 [320, None, None],
@@ -573,7 +610,7 @@ def load_zap_list(archive_name):
 [428, None, None]
 ]
 
-  elif os.path.basename(archive_name) == 'puppi_57646_C0531+33_0085_4275.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57646_C0531+33_0085_4275'):
     zap_list = [\
 [0, None, None],
 [275, None, None],
@@ -587,7 +624,7 @@ def load_zap_list(archive_name):
 [321, None, 500]
 ]
 
-  elif os.path.basename(archive_name) == 'puppi_57638_C0531+33_1218_2797.Tp':
+  elif os.path.basename(archive_name).startswith('puppi_57638_C0531+33_1218_2797'):
     zap_list = [\
 [0, None, None],
 [27, 2400, 3000],
@@ -620,5 +657,5 @@ def load_zap_list(archive_name):
   return zap_list 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__'):
   main()
