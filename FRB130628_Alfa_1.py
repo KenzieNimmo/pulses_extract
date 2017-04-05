57364d7179ff48419483bc3c434763490c14e2fe
Kelly Gourdji <kgourdji@gmail.com>
2017-04-05 17:13:02 +0200

    bug fix FRB130628 pipeline

--- FRB130628_Alfa_1.py
+++ FRB130628_Alfa_1.py
@@ -125,7 +125,7 @@ execute("mkdir pulses")
 execute("%s/pulses_extract.py -db_name %s_b%ds%d_SinglePulses.hdf5 -fits %s\
  		-store_events -idL %s_b%ds%d_ZERO_DM* -store_dir pulses \
 			-plot_pulses -plot_statistics -parameters_id FRB130628_Alfa_s%d > /dev/null"\
-		%(script_dir,base,beam,subband,infile,subband))
+		%(script_dir,base,beam,subband,infile,base,beam,subband,subband))
 		
 """
 #concatenate all singlepulse files to create one master file
